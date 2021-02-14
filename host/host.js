96d70e43f28f336362037f1135fe9562010a3168
blghns <bsaglik@veson.com>
2021-02-14 14:13:28 -0500

    Initial work to get it working.

---
+++ host/host.js
@@ -0,0 +1,137 @@
+(function () {
+
+    let lastPeerId = null;
+    let peer = null; // Own peer object
+    let connections = {};
+    let connectionVoteLookup = {};
+
+    /**
+     * Create the Peer object for our end of the connection.
+     *
+     * Sets up callbacks that handle any events related to our
+     * peer object.
+     */
+    function initialize() {
+        // Create own peer object with connection to shared PeerJS server
+        peer = new Peer(null, {
+            debug: 2
+        });
+
+        peer.on('open', function (id) {
+            // Workaround for peer.reconnect deleting previous id
+            if (peer.id === null) {
+                console.log('Received null id from peer open');
+                peer.id = lastPeerId;
+            } else {
+                lastPeerId = peer.id;
+            }
+
+            console.log('ID: ' + peer.id);
+        });
+        peer.on('connection', function (c) {
+            console.log("Connected to: " + c.peer);
+            ready(c);
+            updateLobby();
+        });
+        peer.on('disconnected', function () {
+            console.log('Connection lost. Please reconnect');
+
+            // Workaround for peer.reconnect deleting previous id
+            peer.id = lastPeerId;
+            peer._lastServerId = lastPeerId;
+            peer.reconnect();
+        });
+        peer.on('close', function () {
+            connections = {};
+            connectionVoteLookup = {};
+            console.log('Connection destroyed');
+        });
+        peer.on('error', function (err) {
+            console.log(err);
+            alert('' + err);
+        });
+
+        document.querySelector("#getVoteLink").addEventListener("click", () => {
+            document.querySelector("#voteLink").value = `${window.location.origin}/PlanningPokerP2P/vote/index.html?hostId=${peer.id}`
+        });
+
+        document.querySelector("#revealButton").addEventListener("click", () => {
+            updateLobby(true);
+        });
+
+        document.querySelector("#clearButton").addEventListener("click", () => {
+            Object.keys(connections).forEach(connectionId => {
+                connectionVoteLookup[connectionId] = {
+                    ...connectionVoteLookup[connectionId],
+                    vote: "",
+                    type: "present"
+                }
+            });
+            updateLobby();
+        });
+    }
+
+    /**
+     * Defines callbacks to handle incoming data and connection events.
+     */
+    function ready(conn) {
+        connections[conn.connectionId] = conn;
+        conn.on('data', function (data) {
+            console.log("Data recieved");
+            console.log(data);
+            connectionVoteLookup[conn.connectionId] = data;
+            updateLobby();
+        });
+        conn.on('close', function () {
+            console.log("Connection reset. Awaiting connection...");
+            delete connections[conn.connectionId];
+            conn = null;
+        });
+    }
+
+    function updateLobby(reveal) {
+
+        const connectionKeys = Object.keys(connections);
+        let total = 0;
+        let count = 0;
+        let votedCount = 0;
+        const tableBody = connectionKeys.map(connectionId => {
+            const { username, vote, type } = connectionVoteLookup[connectionId];
+            const votedText = reveal ? vote : type === "vote" ? "voted" : "present";
+            if (Number.isInteger(+vote)) {
+                total += vote;
+                count++;
+            }
+            if (type === "vote") {
+                votedCount++;
+            }
+            return `<tr>
+                    <td>${username}</td>
+                    <td>${votedText}</td>
+                  </tr>`
+        });
+
+        let votedText = "";
+        if (votedCount === connectionKeys.length) {
+            votedText = `<h3>Everyone voted! 🎉</h3>`;
+        }
+
+        document.querySelector("#lobby").innerHTML = `<table>
+                                                              <tr>
+                                                                <th>User</th>
+                                                                <th>Vote</th>
+                                                              </tr>
+                                                              ${tableBody}
+                                                            </table>${votedText}`;
+
+        let averageText = "waiting to reveal...";
+        if (reveal) {
+            const average = total / count;
+            averageText = isNaN(average) ? "" : `${average}`;
+        }
+        document.querySelector("#average").innerHTML = averageText;
+
+    }
+
+    initialize();
+})();
---
+++ vote/vote.js
@@ -0,0 +1,120 @@
+(function () {
+
+    let lastPeerId = null;
+    let peer = null; // own peer object
+    let conn = null;
+
+    /**
+     * Create the Peer object for our end of the connection.
+     *
+     * Sets up callbacks that handle any events related to our
+     * peer object.
+     */
+    function initialize() {
+        // Create own peer object with connection to shared PeerJS server
+        peer = new Peer(null, {
+            debug: 2
+        });
+
+        peer.on('open', function (id) {
+            // Workaround for peer.reconnect deleting previous id
+            if (peer.id === null) {
+                console.log('Received null id from peer open');
+                peer.id = lastPeerId;
+            } else {
+                lastPeerId = peer.id;
+            }
+
+            console.log('ID: ' + peer.id);
+        });
+        peer.on('connection', function (c) {
+            // Disallow incoming connections
+            c.on('open', function () {
+                c.send("Sender does not accept incoming connections");
+                setTimeout(function () {
+                    c.close();
+                }, 500);
+            });
+        });
+        peer.on('disconnected', function () {
+            console.log('Connection lost. Please reconnect');
+
+            // Workaround for peer.reconnect deleting previous id
+            peer.id = lastPeerId;
+            peer._lastServerId = lastPeerId;
+            peer.reconnect();
+        });
+        peer.on('close', function () {
+            conn = null;
+            console.log('Connection destroyed');
+        });
+        peer.on('error', function (err) {
+            console.log(err);
+            alert('' + err);
+        });
+
+        document.querySelector("#presentButton").addEventListener("click", () => {
+            signal(selectInputs("present"));
+        });
+
+        document.querySelector("#voteButton").addEventListener("click", () => {
+            signal(selectInputs("vote"));
+        });
+    }
+
+    function selectInputs(type) {
+        const username = document.querySelector("#username").value;
+        const vote = document.querySelector("#vote").value;
+        return { username, vote, type };
+    }
+
+    /**
+     * Create the connection between the two Peers.
+     *
+     * Sets up callbacks that handle any events related to the
+     * connection and data received on it.
+     */
+    function join(hostId) {
+        // Close old connection
+        if (conn) {
+            conn.close();
+        }
+
+        // Create connection to destination peer specified in the input field
+        conn = peer.connect(hostId, {
+            reliable: true
+        });
+
+        conn.on('open', function () {
+            console.log("Connected to: " + conn.peer);
+        });
+        // Handle incoming data (messages only since this is the signal sender)
+        conn.on('data', function (data) {
+            console.log(data);
+        });
+    }
+
+    /**
+     * Send a signal via the peer connection and add it to the log.
+     * This will only occur if the connection is still alive.
+     */
+    function signal(s) {
+        if (conn && conn.open) {
+            conn.send(s);
+            console.log(s + " signal sent");
+        } else {
+            console.log('Connection is closed');
+        }
+    }
+
+    // Since all our callbacks are setup, start the process of obtaining an ID
+    initialize();
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const hostId = urlParams.get("hostId");
+    if (hostId != null) {
+        setTimeout(() => join(hostId), 500); // do not join immediately.
+    }
+
+})();
